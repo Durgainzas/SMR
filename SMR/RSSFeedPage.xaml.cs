78bf49992d47cb24156c1c077225a43db7219797
Sebastian Wallo <sebastian.wallo@outlook.com>
2017-11-07 23:40:55 +0100

    remove unnecessary class

--- SMR/RSSFeedPage.xaml.cs
+++ SMR/RSSFeedPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMR.Helpers;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Web.Syndication;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -23,12 +25,36 @@ namespace SMR
     public sealed partial class RSSFeedPage : Page
     {
         DispatcherTimer timer = new DispatcherTimer();
+        SyndicationFeed feed = new SyndicationFeed();
+        RSSClient rssclient = new RSSClient();
+
+
         string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ";
+        private string rssFeedUri = "http://servis.idnes.cz/rss.aspx?c=zpravodaj";
+
 
         public RSSFeedPage()
         {
             this.InitializeComponent();
-            TextBlock.Text = $"\t\t\t\t\t\t\t\t\t\t\t{lorem}\t\t\t\t\t\t\t\t\t\t\t\t\t";
+            Feeder_Init();
+        }
+
+        private async void Feeder_Init()
+        {
+            feed = await rssclient.GetFeedAsync(rssFeedUri);
+            try
+            {
+                foreach (var item in feed.Items)
+                {
+                    TextBlock.Text += $"{item.Title.Text}. ";
+                }
+            }
+            catch (Exception e)
+            {
+                TextBlock.Text = e.Message;
+                throw;
+            }
+
         }
 
         private void ScrollViewer_Loaded(object sender, RoutedEventArgs e)
